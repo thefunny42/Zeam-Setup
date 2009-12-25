dfc67fcb3243812ff80cbacbdb7f343d3377e443
Sylvain Viollon <thefunny@gmail.com>
2009-12-25 11:18:27 +0100

    Add source to build upload commands

---
+++ src/zeam/setup/base/upload.py
@@ -0,0 +1,13 @@
+
+
+class UploadDistribution(object):
+    """Upload a distribution to a package repository.
+    """
+
+    def __init__(self, config, environment):
+        self.config = config
+        self.environment = environment
+
+    def run(self):
+        pass
+
